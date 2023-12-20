50df2c7fdac94d2963a53230eff6af7b8b1b2f18
Markus Zhang <markuszhang8@gmail.com>
2023-12-20 14:08:59 +0800

    more pip requirements

--- scripts/tensordock.sh
+++ scripts/tensordock.sh
@@ -1,4 +1,4 @@
-conda activate tensorml
+conda create -n agi python=3.9 -y
 
 git config --global credential.helper store
 sudo apt install git-lfs
@@ -6,7 +6,8 @@ git lfs install
 
 git clone https://github.com/photomz/outlier-free-transformers.git
 
-echo 'alias skyy="cd ~/outlier-free-transformers && conda activate tensorml"' >> ~/.bashrc
+echo 'alias skyy="cd ~/outlier-free-transformers && conda activate agi"' >> ~/.bashrc
+# echo 'export LD_LIBRARY_PATH=/home/user/mambaforge/condabin/conda:$LD_LIBRARY_PATH' >> ~/.bashrc
 source ~/.bashrc
 
 skyy
@@ -14,8 +15,6 @@ git checkout tensordock
 
 # README install
 skyy
-pip install --upgrade --no-deps pip
 pip install torch==1.13.0 torchvision==0.16.0
 pip install -r docker/requirements.txt
 wandb login
-chmod +x scripts/*.sh
