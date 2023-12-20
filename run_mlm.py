84b1b95e7d4364167875349f38b8c6b824666929
Markus Zhang <markuszhang8@gmail.com>
2023-12-20 19:03:35 +0800

    fix

--- run_mlm.py
+++ run_mlm.py
@@ -392,6 +392,7 @@ def main():
             
             
         def count_tokens(examples):
+            print(examples[0])
             return {'sum': [len(example['input_ids']) for example in examples]}
         
         # We can use .map to .reduce: https://github.com/huggingface/datasets/pull/5533#issuecomment-1440571658
