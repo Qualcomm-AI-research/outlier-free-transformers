c1d9ae07b742732664097deabc484056759e3047
Markus Zhang <markuszhang8@gmail.com>
2023-12-20 14:54:43 +0800

    add apache-beam to parse wikipedia

--- scripts/tensordock.sh
+++ scripts/tensordock.sh
@@ -7,6 +7,7 @@ git lfs install
 git clone https://github.com/photomz/outlier-free-transformers.git
 
 echo 'alias skyy="cd ~/outlier-free-transformers && conda activate agi"' >> ~/.bashrc
+echo 'export OMP_NUM_THREADS=$(($(nproc) - 2))' >> ~/.bashrc # num CPU cores-1
 # echo 'export LD_LIBRARY_PATH=/home/user/mambaforge/condabin/conda:$LD_LIBRARY_PATH' >> ~/.bashrc
 source ~/.bashrc
 
